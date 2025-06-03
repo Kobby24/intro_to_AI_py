bb0a8d95bcd82372c9e43352f811f00db0402e41
Kobby24 <geffah005@st.ug.edu.gh>
2025-06-03 15:17:59 +0000

    maze

--- maze.py
+++ maze.py
@@ -61,5 +61,84 @@ class Maze:
                         row.append(False)
                     elif contents[i][j] == "B":
                         self.goal=(i,j)
+                        row.append(False)
                     elif contents[i][j] == " ":
+                        row.append(False)
+                    else:
+                        row.append(True)
+                except IndexError:
+                    row.append(False)
+            self.walls.append(row)
+        self.solution = None
+    def print(self):
+        solution = self.solution[1] if self.solution is not None else None
+        print()
+        for i,row in enumerate(self.walls):
+            for j,col in enumerate(row):
+                if col:
+                    print(" ",end="")
+                elif (i,j) == self.goal:
+                    print("B",end="")
+                elif solution is not None and (i,j) in solution:
+                    print("*",end="")
+                else:
+                    print(" ",end="")
+            print()
+        print()
+
+
+    def neighbours(self,state):
+        row, col = state
+        candidates = [
+            ("up",(row - 1,col)),
+            ("down",(row + 1,col)),
+            ("left",(row,col - 1)),
+            ("right",(row,col +1))
+        ]
+        result = []
+        for action, (r,c) in candidates:
+            try:
+                if not self.walls[r][c]:
+                    result.append((action,(r,c)))
+            except IndexError:
+                continue
+        return result
+
+    def solve(self):
+        self.num_explored = 0
+        start = Node(state=self.start,parent=None,action=None)
+        frontier = StackFrontier()
+        frontier.add(start)
+        self.explored = set()
+
+        while True:
+            if frontier.empty():
+                raise Exception("no solution")
+            node = frontier.remove()
+            self.num_explored +=1
+            if node.state == self.goal:
+                actions = []
+                cells = []
+
+                while node.parent is not None:
+                    actions.append(node.action)
+                    cells.append(node.state)
+                actions.reverse()
+                cells.reverse()
+                self.solution = (actions,cells)
+                return
+
+
+            self.explored.add(node.state)
+
+            for action,state in self.neighbours(node.state):
+                if not frontier.contains_state(state) and state not in self.explored:
+                    child = Node(state=state,parent=node,action=action)
+
+        output_image(self,filename,show_solution=True,show_explored=False)
+
+
+
+
+
 
